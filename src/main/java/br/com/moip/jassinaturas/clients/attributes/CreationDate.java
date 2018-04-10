6cd6f45d9d4ec6e241dd406ec7f8a1245cd722b5
Odravison <odravison.amaral@dce.ufpb.br>
2018-04-10 19:39:01 -0300

    Refactors of creationDate

--- src/main/java/br/com/moip/jassinaturas/clients/attributes/CreationDate.java
+++ src/main/java/br/com/moip/jassinaturas/clients/attributes/CreationDate.java
@@ -31,6 +31,30 @@ public class CreationDate {
     public int getYear() {
         return year;
     }
+    
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+    	this.minute = minute;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public void setSecond(int second) {
+    	this.second = second;
+    }
+
+    public void setYear(int year) {
+    	this.year = year;
+    }
 
     @Override
     public String toString() {
